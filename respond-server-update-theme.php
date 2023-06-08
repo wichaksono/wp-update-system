57f4a3827c15e5a60f11311865c25050cf9fddbe
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2023-06-08 23:19:57 +0700

    Update respond-server-update-theme.php

--- respond-server-update-theme.php
+++ respond-server-update-theme.php
@@ -1,13 +1,50 @@
 <?php
 
-$response = array(
-	'theme'        => 'theme-name',
-  'new_version'  => '1.1',
-  'url'          => 'https://neon.web.id/produk/theme-name',
-  'package'      => 'https://neon.web.id/produk/theme-name-1.zip',
-  'requires'     => '',
-  'requires_php' => '',
-);
+const THEME_NAME = 'gpinfo';
+const THEME_VERSION = '1.0';
+const THEME_REPOSITORY = 'https://gpblog.skydia.com/update/theme';
 
-header('Content-Type: application/json');
-echo json_encode($response, 128);
+function neon_fetch_new_theme() {
+  $server_update = THEME_REPOSITORY . '/update.php?slug=' . THEME_NAME;
+  
+  $response = wp_remote_get($server_update);
+  
+  if ( is_array( $response ) && ! is_wp_error( $response ) ) {
+      $object_response = json_decode( $response['body'], true );
+      
+      if ( json_last_error() === JSON_ERROR_NONE ) {
+           return $object_response;
+      }         
+  }
+  
+  return [];
+}
+
+function neon_update_theme( $transient ) {
+    
+
+    // Query premium/private repo for updates.
+    $update = neon_fetch_new_theme();
+    if ( $update ) {
+        // Update is available.
+      
+        $transient->response[THEME_NAME] = $update;
+    } else {
+        // No update is available.
+        $item = array(
+            'theme'        => THEME_NAME,
+            'new_version'  => $current_verion,
+            'url'          => '',
+            'package'      => '',
+            'requires'     => '',
+            'requires_php' => '',
+        );
+        
+        // set to no update available
+        $transient->no_update[THEME_NAME] = $item;
+    }
+ 
+    return $transient;
+}
+ 
+add_filter( 'pre_set_site_transient_update_themes', 'neon_update_theme' );
